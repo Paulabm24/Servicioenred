cfb52f8665a7114e7cc51db58612f932e704e721
Paulabm24 <114907123+Paulabm24@users.noreply.github.com>
2023-10-10 10:10:35 +0200

    Update dhcp.sh

--- dhcp.sh
+++ dhcp.sh
@@ -1,3 +1,12 @@
+!#/bin/bash
 apt update
 apt upgrade
 apt install isc-dhcp-server
+#Configuracion de la red
+
+#Configuracion tarjeta
+
+#Configuracion dhcp
+
+systemctl restart isc-dhcp-server
+systemctl status isc-dhcp-server
