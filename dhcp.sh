09d14c1c41a0441d078711033e17b132661388e3
Paulabm24 <114907123+Paulabm24@users.noreply.github.com>
2023-10-16 13:32:35 +0200

    Update dhcp.sh

--- dhcp.sh
+++ dhcp.sh
@@ -6,8 +6,14 @@ apt install isc-dhcp-server
 
 #Configuracion tarjeta
 sudo bash -c 'cat << EOF > /etc/default/isc-dhcp-server
-
+# On what interfaces should the DHCP server (dhcpd) serve DHCP requests?
+#	Separate multiple interfaces with spaces, e.g. "eth0 eth1".
+INTERFACESv4="enp0s8"
+INTERFACESv6=""
+EOF
 #Configuracion dhcp
+sudo bash -c 'cat << EOF > /etc/dhcp/dhcp.conf
+
 
 systemctl restart isc-dhcp-server
 systemctl status isc-dhcp-server
