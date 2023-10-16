06c5f59d0a1784ab9e05c4e3ece61fee5a677085
Paulabm24 <114907123+Paulabm24@users.noreply.github.com>
2023-10-16 13:33:38 +0200

    Update dhcp.sh

--- dhcp.sh
+++ dhcp.sh
@@ -3,6 +3,8 @@ apt update
 apt upgrade
 apt install isc-dhcp-server
 #Configuracion de la red
+sudo bash -c 'cat << EOF > /etc/network/interfaces
+
 
 #Configuracion tarjeta
 sudo bash -c 'cat << EOF > /etc/default/isc-dhcp-server
@@ -13,7 +15,33 @@ INTERFACESv6=""
 EOF
 #Configuracion dhcp
 sudo bash -c 'cat << EOF > /etc/dhcp/dhcp.conf
+authoritative;
+ddns-update-style none;
 
+failover peer "FAILOVER" {
+  primary; 
+  address 192.168.2.3;
+  port 647;
+  peer address 192.168.2.4;
+  peer port 647;
+  max-unacked-updates 10;
+  max-response-delay 30;
+  load balance max seconds 3;
+  mclt 1800;
+  split 128;
+}
+
+subnet 192.168.2.0 netmask 255.255.255.0 {
+  option broadcast-address 192.168.2.255;
+  option routers 192.168.2.1;
+  option domain-name-servers 8.8.8.8, 8.8.4.4;
+  pool {
+    failover peer "FAILOVER";
+    max-lease-time 3600;
+    range 192.168.2.13 192.168.2.16;
+  }
+}
+EOF
 
 systemctl restart isc-dhcp-server
 systemctl status isc-dhcp-server
